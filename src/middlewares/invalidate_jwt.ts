ee5880df81f42c50b51584e66017364b682daf81
hyperingenious <skbmasale941@gmail.com>
2025-05-19 22:18:20 +0530

    convert: invalidate_jwt

---
+++ src/middlewares/invalidate_jwt.ts
@@ -0,0 +1,21 @@
+import { invalidateToken } from "../helpers/helper";
+import { Request, Response, NextFunction } from "express";
+
+async function invalidateJwt(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const verifiedToken = await invalidateToken({ req, res });
+    // @ts-ignore
+    req.verifiedToken = verifiedToken;
+    next();
+  } catch (error) {
+    res
+      .status(401)
+      .json({ error: "Unauthorized", message: "Cannot authenticate you" });
+  }
+}
+
+export default invalidateJwt;
