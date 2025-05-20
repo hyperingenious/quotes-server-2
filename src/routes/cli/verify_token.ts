3a909b2e1b6b2f83b088790bfb13ef49f0f8f911
hyperingenious <skbmasale941@gmail.com>
2025-05-21 05:21:32 +0530

    modify verify token

--- src/routes/cli/verify_token.ts
+++ src/routes/cli/verify_token.ts
@@ -1,23 +1,26 @@
 import { verify_token } from "../../appwrite/verify/verify_appwrite";
 import { Request, Response } from "express";
 
-async function verifyToken(req: Request, res: Response) {
+async function verifyToken({ req, res }: { req: Request; res: Response }) {
   const token = req.body.token;
   console.log(token);
 
   if (!token) {
-    return res.status(400).json({ error: "No token found" });
+    res.status(400).json({ error: "No token found" });
+    return 
   }
 
   try {
     // Call the imported function to verify the token
     const { isTokenValid, related_data } = await verify_token({ token });
     // Respond with the result of the verification
-    return res.status(200).json({ isTokenValid, related_data });
+    res.status(200).json({ isTokenValid, related_data });
+    return 
   } catch (error) {
     // Handle any errors during token verification
     console.error("Error verifying token:", error);
-    return res.status(500).json({ error: "Internal server error" });
+     res.status(500).json({ error: "Internal server error" });
+    return
   }
 }
 
